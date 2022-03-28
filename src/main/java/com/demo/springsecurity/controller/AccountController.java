1c9943616fb6ef8bde69c4c83b6a1a5bad38c848
EViziteu <elenamadalina.viziteu@endava.com>
2022-03-28 22:30:56 +0300

    add account

--- src/main/java/com/demo/springsecurity/controller/AccountController.java
+++ src/main/java/com/demo/springsecurity/controller/AccountController.java
@@ -1,6 +1,9 @@
 package com.demo.springsecurity.controller;
 
+import com.demo.springsecurity.model.Account;
 import com.demo.springsecurity.model.Transaction;
+import com.demo.springsecurity.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class AccountController {
 
+    @Autowired
+    AccountService accountService;
+
     @GetMapping(value = "/accounts")
     public String getAccounts() {
         return " accounts ";
+    }
+
+    @PostMapping("/account/add")
+    public void createAccount(@RequestBody Account account){
+        accountService.addAccount(account);
     }
 
 }
--- src/main/java/com/demo/springsecurity/service/AccountService.java
+++ src/main/java/com/demo/springsecurity/service/AccountService.java
@@ -1,15 +1,13 @@
 package com.demo.springsecurity.service;
 
-import com.demo.springsecurity.dto.AccountDto;
 import com.demo.springsecurity.mapper.AccountMapper;
 import com.demo.springsecurity.model.Account;
-import com.demo.springsecurity.model.User;
 import com.demo.springsecurity.repository.AccountRepository;
 import com.demo.springsecurity.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
+@Service
 public class AccountService {
 
     @Autowired
@@ -19,9 +17,13 @@ public class AccountService {
     @Autowired
     AccountRepository accountRepository;
 
-    public void addAccount(AccountDto accountDto, String userEmail){
-        Optional<User> optionalUser = userRepository.findByEmail(userEmail);
-        Account account = accountMapper.map(accountDto, optionalUser.get());
+//    public void addAccount(AccountDto accountDto, String userEmail){
+//        Optional<User> optionalUser = userRepository.findByEmail(userEmail);
+//        Account account = accountMapper.map(accountDto, optionalUser.get());
+//        accountRepository.save(account);
+//    }
+
+    public void addAccount(Account account) {
         accountRepository.save(account);
     }
 }
